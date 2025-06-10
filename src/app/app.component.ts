7658a2e82910f22f00cce050f2ec6a61b9478064
Usman Mehmood <usmanmehmood@Usmans-MacBook-Pro.local>
2025-06-09 21:20:56 -0400

    add comments for futher work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,5 +23,13 @@ export class AppComponent {
   setSessionDetails(details: SessionInfo) {
     this.startTime = details.startTime.getHours();
     this.endTime = details.endTime.getHours();
+
+    // have to find way to populate list of {from, to} objects from this for chart component to consume
+    // without any breaks, very simple --> only one object from startTime to endTime
+    // with breaks, there have to be multiple objects for working sessions and multiple objects for breaks
+
+    // also have to think about adding a slider on the chart component for the current minute
+    // also have to think about adding hover functionality so that user knows what block of time
+    // corresponds to what (ex. hovering over working session 1 along with details like number of cases in this block)
   } 
 }
